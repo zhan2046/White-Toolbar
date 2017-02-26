6f69ae94f836ebf491523128a6c4924ed5e81fe2
ruzhan123 <dev19921116@gmail.com>
2017-02-26 10:35:10 +0800

    add white toolbar

--- app/src/main/java/zhan/whitetoolbar/MainActivity.java
+++ app/src/main/java/zhan/whitetoolbar/MainActivity.java
@@ -2,11 +2,26 @@ package zhan.whitetoolbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+  protected Toolbar toolbar;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    toolbar = (Toolbar) findViewById(R.id.toolbar);
+    toolbar.setTitle("White Toolbar");
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    getSupportActionBar().setDisplayShowTitleEnabled(true);
+    toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        finish();
+      }
+    });
   }
 }
